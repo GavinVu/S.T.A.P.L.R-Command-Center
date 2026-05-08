27453adf700d5ba8b4c6918399ff280eeade142b
Brixablox <Brixablox@gmail.com>
2026-05-07 19:38:58 -0700

    Add Supabase project config

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.STAPLR_SUPABASE_CONFIG = {
-  url: "https://YOUR-PROJECT-REF.supabase.co",
-  anonKey: "YOUR-SUPABASE-ANON-PUBLIC-KEY"
+  url: "https://gdcgogioftuhpkxljlkb.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdkY2dvZ2lvZnR1aHBreGxqbGtiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgxOTk5NjcsImV4cCI6MjA5Mzc3NTk2N30._qJ-fFdY1HTpV9cCgaBVOqjZT59womAs5PB0khIl-A0"
 };
